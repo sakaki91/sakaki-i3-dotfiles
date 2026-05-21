580860825a287751ff2171073b989705b53dc48e
sakaki <sakaki@tuta.io>
2026-05-21 14:41:17 -0300

    Update 0.5 - Kitty & Rofi Config's and auto-installer Added!

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo teste
+
+dotfiles=$(pwd)
+set -x
+cp -rT "$dotfiles"/user/.bashrc "$HOME"/.bashrc
+cp -rT "$dotfiles"/user/.bash_profile "$HOME"/.bash_profile
+cp -rT "$dotfiles"/user/.local/bin/auto-proton "$HOME"/.local/bin/auto-proton
+cp -rT "$dotfiles"/user/.config/rofi "$HOME"/.config/rofi
+cp -rT "$dotfiles"/user/.config/kitty "$HOME"/.config/kitty
