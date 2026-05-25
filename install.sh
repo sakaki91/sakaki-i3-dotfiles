3959db9f45863cf30cb403d26d2ad629ac32c100
sakaki <sakaki@tuta.io>
2026-05-25 04:32:37 -0300

    Update

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ cp -rT "$dotfiles"/user/.config/rofi "$HOME"/.config/rofi
 cp -rT "$dotfiles"/user/.config/kitty "$HOME"/.config/kitty
 cp -rT "$dotfiles"/user/.config/i3 "$HOME"/.config/i3
 cp -rT "$dotfiles"/user/.config/i3blocks "$HOME"/.config/i3blocks
-cp -rT "$dotfiles"/user/.local/bin/auto-proton "$HOME"/.local/bin/auto-proton
+cp -rT "$dotfiles"/user/.local/bin/protonmanager "$HOME"/.local/bin/protonmanager
+cp -rT "$dotfiles"/user/.local/bin/slackmount "$HOME"/.local/bin/slackmount
+cp -rT "$dotfiles"/user/.local/bin/basicc "$HOME"/.local/bin/basicc
